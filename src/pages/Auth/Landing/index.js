241aea0fd98a6781082178278b0761e318a50341
Paulo Victor de Menezes Lopes <pv.paulovictor.vp@gmail.com>
2020-07-16 20:45:49 -0300

    Update Landing Page text

--- src/pages/Auth/Landing/index.js
+++ src/pages/Auth/Landing/index.js
@@ -10,30 +10,30 @@ class Landing extends React.Component {
       </div>
       <div className="content">
         <p>
-          Este projeto visa disponibilizar treinos para quem gostaría de obter uma vida mais saudável, mas não tem condições de
-          frequentar uma academia regularmente. Seja a academia, em um parque ou em sua residência, com vários equipamentos ou
-          nenhum, é possível fazer atividade física e queremos te auxiliar.
+          Nosso projeto visa disponibilizar treinos para quem gostaria de obter uma vida mais saudável, mas não tem condições ou
+          tempo disponível para frequentar uma academia regularmente. Seja na academia, em um parque ou em sua residência, com
+          vários equipamentos ou nenhum, é possível fazer atividade física e queremos te auxiliar.
         </p>
       </div>
 
       <div className="title">
-          <h1>Como participar!</h1>
+          <h1>Como participar?</h1>
       </div>
       <div className="content">
         <p>
-          Muito fácil! Basta fazer o download do nosso App, e fazer seu cadastro como aluno e seguir 2 passos que o aplicativo
+          Muito fácil! Basta fazer o download do nosso App, fazer seu cadastro como aluno e seguir os passos que o aplicativo
           te indicará. Com isso sua ficha será encaminhada a um de nossos professores, que por sua vez fará um treinamento
-          perfeito para as suas condições e pretensões.
+          perfeito para as suas pretensões e condições.
         </p>
       </div>
 
       <div className="title">
-        <h1>Quem somos nós!</h1>
+        <h1>Quem somos nós...</h1>
       </div>
       <div className="content">
         <p>
-          Somos todos graduandos com um objetivo em comum: permitir um estilo de vida mais saudável para todos, em quaisquer
-          condições. Vemos esse projeto como um forma para ganharmos experiência no mercado de trabalho além de estamos
+          Somos graduandos com um objetivo em comum: permitir um estilo de vida mais saudável para todos, em quaisquer
+          condições. Vemos esse projeto como um forma para ganharmos experiência no mercado de trabalho além de estarmos
           apostando nesse mercado virtual e mais acessível. Clique aqui para ver mais detalhes do nosso grupo!
         </p>
       </div>
