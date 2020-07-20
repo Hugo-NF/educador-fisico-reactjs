3ce30c0d6e22e5f97f39637b0ccbc29ddbfeb0a1
Matheus Amaral <mam.macacod@gmail.com>
2020-07-20 02:54:13 -0300

    integration with the back-end

--- src/pages/Auth/Register/index.js
+++ src/pages/Auth/Register/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./style.scss";
+import api from "../../../services/api";
+import InputMask from "react-input-mask";
 
 import {
   Button,
@@ -24,36 +26,62 @@ class Register extends React.Component {
       name : "",
       email : "",
       password : "",
-      birthdate : new Date(),
+      birthDate : new Date(),
       sex : "",
       phoneType: "", 
       phoneNumber : "",
       city : "",
       state : "",
-      checked: false
+      validation: "",
+      checked: false,
+      concidence: {text : "não", color: "text-danger"}
     };
   }
 
-  cadastrar = (e) => {
+  passwordValidate = event => {
+    if(this.state.password === this.state.validation){
+      console.log("valido");
+    this.setState({concidence: {text: "sim", color: "text-success"}})
+    }else{
+    this.setState({concidence: {text : "não", color: "text-danger"}})
+    }
+  }
 
-    e.preventDefault();
+  handleRegister = async (event) => {
+    event.preventDefault();
+    await api.post('/users/register', {
+      name : this.state.name,
+      email : this.state.email,
+      password : this.state.password,
+      birthDate : this.state.birthDate,
+      sex : this.state.sex,
+      phone: {type : this.state.phoneType, number : this.state.phoneNumber}, 
+      city : this.state.city,
+      state : this.state.state,
+    })
+    .then(response => {
+      console.log(response.status);
+    })
+    .catch(erro => {
+      console.log(erro.response.status)
+    })
   }
 
   render() {
     return (
       <div className="Register">
         <Col lg="6" md="8">
-          <Card className="bg-secondary shadow border-0">
+          <Card className="bg-secondary" id="register-card">
             <CardBody className="px-lg-5 py-lg-5">
               <div className="text-center text-muted mb-4">
                 <h2>Cadastre-se.</h2>
                 <small> Preencha os campos necessários</small>
               </div>
 
-              {/*  name, email, password, birthdate, sex, phones[type, number], city, state */}
+              {/*  name, email, password, birthDate, sex, phones[type, number], city, state */}
 
 
-              <Form role="form" onSubmit={event => this.cadastrar(event)}>
+              <Form role="form" onSubmit={event => this.handleRegister(event)}>
                 <FormGroup>
                   <label className="form-control-label" htmlFor="input-country">
                     Nome
@@ -92,7 +120,7 @@ class Register extends React.Component {
                         <i className="ni ni-lock-circle-open" />
                       </InputGroupText>
                     </InputGroupAddon>
-                    <Input placeholder="Senha" type="password" autoComplete="new-password" onChange={event => this.setState({password : event.target.value})}/>
+                    <Input placeholder="Senha" type="password" autoComplete="new-password" onKeyUp={event => { this.passwordValidate(event);}} onChange={event => {this.setState({password : event.target.value});}}/>
                   </InputGroup>
                 </FormGroup>
 
@@ -106,10 +134,17 @@ class Register extends React.Component {
                         <i className="ni ni-lock-circle-open" />
                       </InputGroupText>
                     </InputGroupAddon>
-                    <Input placeholder="Confirmar" type="password" />
+                    <Input placeholder="Confirmar" type="password" onKeyUp={event => this.passwordValidate(event)} onChange={event => {this.setState({validation : event.target.value});}}/>
                   </InputGroup>
                 </FormGroup>
-                
+                                
+                <div className="text-muted font-italic mb-3 mt--3">
+                  <small>
+                    senhas coincidem:{" "}
+                    <span className={`${this.state.concidence.color} font-weight-700`}>{this.state.concidence.text}</span>
+                  </small>
+                </div>
+
                 <Row>
                   <Col xs="6" sm="6">
                     <FormGroup>
@@ -176,7 +211,7 @@ class Register extends React.Component {
                         Numero
                       </label>
                       <InputGroup className="input-group-alternative mb-3">
-                        <Input placeholder="Telefone" type="telephone" onChange={event => this.setState({phoneNumber : event.target.value})}/>
+                        <InputMask id="mask" placeholder="Telefone" mask="+55 (99) 99999-9999" onChange={event => this.setState({phoneNumber : event.target.value})}></InputMask>
                       </InputGroup>
                     </FormGroup>
                   </Col>
@@ -209,7 +244,7 @@ class Register extends React.Component {
                             <i className="ni ni-square-pin" />
                           </InputGroupText>
                         </InputGroupAddon>
-                        <Input placeholder="Estado" type="select" onChange={event => this.setState({state : event.target.value})}>
+                        <Input  placeholder="Estado" type="select" onChange={event => this.setState({state : event.target.value})}>
                           <option hidden>Estado</option>
                           <option value="AC">AC</option> <option value="AL">AL</option> <option value="AP">AP</option>
                           <option value="AM">AM</option> <option value="BA">BA</option> <option value="CE">CE</option>
@@ -225,12 +260,7 @@ class Register extends React.Component {
                     </FormGroup>
                   </Col>
                 </Row>
-                <div className="text-muted font-italic">
-                  <small>
-                    password strength:{" "}
-                    <span className="text-success font-weight-700">strong</span>
-                  </small>
-                </div>
+                
                 <Row className="my-4">
                   <Col xs="12">
                     <div className="custom-control custom-control-alternative custom-checkbox">
