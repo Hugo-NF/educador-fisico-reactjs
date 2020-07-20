d34959d2c228587af1f09b5269687b51bec4a3ab
Ailamar Alves <ailamar.alvesg@gmail.com>
2020-07-19 21:54:40 -0300

    Add prototype of images and background

--- src/pages/Auth/Landing/index.js
+++ src/pages/Auth/Landing/index.js
@@ -4,26 +4,31 @@ import "./style.scss";
 class Landing extends React.Component {
   render() {
     return (
-    <div className="landing-page">
-      <div className="title">
-        <h1>Treino para todos!</h1>
-      </div>
+    <div className="landing-page">          
       <div className="content">
-        <p>
-          Nosso projeto visa disponibilizar treinos para quem gostaria de obter uma vida mais saudável, mas não tem condições ou
-          tempo disponível para frequentar uma academia regularmente. Seja na academia, em um parque ou em sua residência, com
-          vários equipamentos ou nenhum, é possível fazer atividade física e queremos te auxiliar.
+        <div className="background" style={{backgroundImage:"url(" + require("assets/img/landingPage/alongando.jpg") + ")"}}>
+        <div className="background-title">
+          <h1><b>TREINO<br></br>PARA<br></br>TODOS</b></h1>
+        </div>  
+        <p className="background-text">
+          Nosso projeto visa disponibilizar treinos para quem gostaria de obter uma vida mais saudável mas não tem condições ou
+          tempo disponível para frequentar uma academia regularmente. <br></br>
+          Seja na academia, em um parque ou em sua residência, com vários equipamentos ou nenhum, é possível fazer atividade 
+          física e queremos te auxiliar.
         </p>
+        </div>        
       </div>
 
       <div className="title">
           <h1>Como participar?</h1>
       </div>
       <div className="content">
+        <img className="imageslayout" src={require("assets/img/landingPage/abdominal.jpg")} alt="exercicio abdominal"></img>
         <p>
-          Muito fácil! Basta fazer o download do nosso App, fazer seu cadastro como aluno e seguir os passos que o aplicativo
-          te indicará. Com isso sua ficha será encaminhada a um de nossos professores, que por sua vez fará um treinamento
-          perfeito para as suas pretensões e condições.
+          Para participar desse projeto com a gente é muito fácil! Basta fazer o download do nosso app, se cadastrar como aluno 
+          seguir os passos que o aplicativo te indicará e pronto. <br></br> Após o cadastro sua ficha será encaminhada a um de 
+          nossos professores, e criaremos um treinamento perfeito especialmente para as suas pretensões e condições. <br></br>
+          A melhor parte é que você pode se exercitar sem sair de casa!
         </p>
       </div>
 
@@ -31,10 +36,11 @@ class Landing extends React.Component {
         <h1>Quem somos nós...</h1>
       </div>
       <div className="content">
+        <img className="imageslayout" src={require("assets/img/landingPage/afundo.jpg")} alt="exercicio afundo"></img>
         <p>
           Somos graduandos com um objetivo em comum: permitir um estilo de vida mais saudável para todos, em quaisquer
-          condições. Vemos esse projeto como um forma para ganharmos experiência no mercado de trabalho além de estarmos
-          apostando nesse mercado virtual e mais acessível. Clique aqui para ver mais detalhes do nosso grupo!
+          condições.<br></br> Vemos esse projeto como um forma para ganharmos experiência no mercado de trabalho além de 
+          estarmos apostando nesse mercado virtual e mais acessível. Clique aqui para ver mais detalhes do nosso grupo!
         </p>
       </div>
     </div>
