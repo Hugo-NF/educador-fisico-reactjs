b29bf515f20324167817ed2caaefe547d80ace5b
Hugo-NF <hugonfonseca@hotmail.com>
2020-08-08 15:06:47 -0300

    Use of Redirect from router dom

--- src/layouts/Auth.jsx
+++ src/layouts/Auth.jsx
@@ -23,11 +23,11 @@ export default function Auth() {
     return (<prop.component {...props} />);
   };
 
-  const getRoutes = () => routes.map((route) => (
+  const getRoutes = () => routes.map((prop, key) => (
     <Route
-      path={route.layout + route.path}
-      render={(props) => renderPage(props, route)}
-      key={route.key}
+      path={prop.layout + prop.path}
+      render={(props) => renderPage(props, prop)}
+      key={key}
     />
   ));
 
